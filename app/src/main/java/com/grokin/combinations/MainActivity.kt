4269569d70f998f0ea6177be6810a4b1137e2db8
Eoghan Doyle <eoghanbdoyle@gmail.com>
2023-07-30 18:32:38 +0100

    Working but with layout issues.

--- app/src/main/java/com/grokin/combinations/MainActivity.kt
+++ app/src/main/java/com/grokin/combinations/MainActivity.kt
@@ -3,8 +3,12 @@ package com.grokin.combinations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,9 +17,51 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.grokin.combinations.ui.theme.CombinationsTheme
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.chargemap.compose.numberpicker.*
 
 class MainActivity : ComponentActivity() {
+    class SquaresViewModel : ViewModel() {
+        private val _squares = MutableLiveData(2)
+        val squares: LiveData<Int> = _squares
+        private val _total = MutableLiveData(3)
+        val total: LiveData<Int> = _total
+        private val _min = MutableLiveData(3)
+        val min: LiveData<Int> = _min
+        private val _max = MutableLiveData(16)
+        val max: LiveData<Int> = _max
+
+        fun onSquaresChange(newSquares: Int) {
+            _squares.value = newSquares
+
+            var min = 0
+            var max = 0
+            for (i in 1..newSquares) {
+                min += i
+                max += (10 - i)
+            }
+            _total.value?.let {
+                if (min > it) {
+                    _total.value = min
+                }
+                if (max < it) {
+                    _total.value = max
+                }
+                _min.value = min
+                _max.value = max
+            }
+        }
+
+        fun onTotalChange(newTotal: Int) {
+            _total.value = newTotal
+        }
+    }
+
+    private val squaresViewModel = SquaresViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,29 +71,54 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val res = combinations( 3, 14, 1)
-                    val builder = StringBuilder()
-                    for (c in res){
-                        for (n in c){
-                            builder.append(n)
-                        }
-                        builder.append(",")
-                    }
-                    Greeting(builder.toString())
 
+                    val sq by squaresViewModel.squares.observeAsState(2)
+                    val to by squaresViewModel.total.observeAsState(3)
+                    val min by squaresViewModel.min.observeAsState(3)
+                    val max by squaresViewModel.max.observeAsState(16)
+
+
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Column(
+                            Modifier
+                                .padding(16.dp)
+                        ) {
+                            SquaresPicker(sq) { squaresViewModel.onSquaresChange(it) }
+                        }
+                        Column(
+                            Modifier
+                                .padding(16.dp)
+                        ) {
+                            TotalPicker(to, min, max) { squaresViewModel.onTotalChange(it) }
+                        }
+                    }
                     Row {
-                        SquaresPicker()
-                        TotalPicker()
+                        val res = combinations(sq, to)
+                        val builder = StringBuilder("boogie")
+                        for (c in res) {
+                            for (n in c) {
+                                builder.append(n)
+                            }
+                            builder.append(",")
+                        }
+                        Greeting(builder.toString())
+
                     }
                 }
             }
         }
     }
 
-    private fun combinations(squares: Int, total: Int, start: Int): List<List<Int>> {
+
+    private fun combinations(squares: Int, total: Int, start: Int = 1): List<List<Int>> {
         val list = mutableListOf<List<Int>>()
         if (squares == 1) {
-            if (total in (start + 1)..9) {
+            if (total in start..9) {
                 val element = listOf(total)
                 list.add(element)
             }
@@ -64,6 +135,25 @@ class MainActivity : ComponentActivity() {
         }
         return list
     }
+
+    @Composable
+    private fun SquaresPicker(value: Int, onSquaresChange: (Int) -> Unit) {
+        NumberPicker(
+            value = value,
+            range = 2..8,
+            onValueChange = onSquaresChange
+        )
+    }
+
+    @Composable
+    private fun TotalPicker(value: Int, min: Int, max: Int, onTotalChange: (Int) -> Unit) {
+        NumberPicker(
+            value = value,
+            range = min..max,
+            onValueChange = onTotalChange
+        )
+    }
+
 }
 
 @Composable
@@ -80,27 +170,4 @@ fun GreetingPreview() {
     CombinationsTheme {
         Greeting("Android")
     }
-}
-
-@Composable
-private fun SquaresPicker() {
-    var state by remember { mutableStateOf(0) }
-    NumberPicker(
-        value = state,
-        range = 1..9,
-        onValueChange = {
-            state = it
-        }
-    )
-}
-@Composable
-private fun TotalPicker() {
-    var state by remember { mutableStateOf(0) }
-    NumberPicker(
-        value = state,
-        range = 1..9,
-        onValueChange = {
-            state = it
-        }
-    )
 }
